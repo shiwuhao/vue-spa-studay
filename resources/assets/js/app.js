aa4b84420430ab20e849d236b27ead347667c37d
石武浩 <shiwuhao@juzifenqi.com>
2017-10-08 11:42:57 +0800

    all

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,9 +11,18 @@ window.Vue = require('vue');
 
 import VueRouter from  'vue-router';
 import router from './routes';
-import App from './components/App';
+import App from './components/App.vue';
+
+// import zh_CN from 'vee-validate/dist/locale/zh_CN';
+import zh_CN from './locale/zh_CN';
+import VeeValidate, { Validator } from 'vee-validate';
+
+Validator.addLocale(zh_CN);
 
 Vue.use(VueRouter);
+Vue.use(VeeValidate, {
+    locale:'zh_CN'
+});
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/assets/js/components/register/RegisterForm.vue
+++ resources/assets/js/components/register/RegisterForm.vue
@@ -1,40 +1,51 @@
 <template>
     <form class="form-horizontal" method="POST" @submit.prevent="register">
 
-        <div class="form-group">
-            <label for="name" class="col-md-4 control-label">Name</label>
+        <div class="form-group" :class="{ 'has-error' : errors.has('name') }">
+            <label for="name" class="col-md-3 control-label">Name</label>
+            <div class="col-md-7">
+                <input v-model="name"
+                       v-validate data-vv-rules="required|min:4" data-vv-as="用户名"
+                       id="name" type="text" class="form-control" name="name" value="" required autofocus>
+                <span class="help-block" v-show="errors.has('name')">{{ errors.first('name') }}</span>
+            </div>
+        </div>
 
-            <div class="col-md-6">
-                <input v-model="name" id="name" type="text" class="form-control" name="name" value="" required autofocus>
+        <div class="form-group" :class="{ 'has-error' : errors.has('email') }">
+            <label for="email" class="col-md-3 control-label">E-Mail Address</label>
+
+            <div class="col-md-7">
+                <input v-model="email"
+                       v-validate data-vv-rules="required|email" data-vv-as="邮箱"
+                       id="email" type="email" class="form-control" name="email" value="" required>
+                <span class="help-block" v-show="errors.has('email')">{{ errors.first('email') }}</span>
+            </div>
+        </div>
+
+        <div class="form-group" :class="{ 'has-error' : errors.has('password') }">
+            <label for="password" class="col-md-3 control-label">Password</label>
+
+            <div class="col-md-7">
+                <input v-model="password"
+                       v-validate data-vv-rules="required|min:6" data-vv-as="密码"
+                       id="password" type="password" class="form-control" name="password" required>
+                <span class="help-block" v-show="errors.has('password')">{{ errors.first('password') }}</span>
+            </div>
+        </div>
+
+        <div class="form-group" :class="{ 'has-error' : errors.has('password_confirmation') }">
+            <label for="password-confirm" class="col-md-3 control-label">Confirm Password</label>
+
+            <div class="col-md-7">
+                <input id="password-confirm"
+                       v-validate data-vv-rules="required|min:6|confirmed:password" data-vv-as="确认密码"
+                       type="password" class="form-control" name="password_confirmation" required>
+                <span class="help-block" v-show="errors.has('password_confirmation')">{{ errors.first('password_confirmation') }}</span>
             </div>
         </div>
 
         <div class="form-group">
-            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-            <div class="col-md-6">
-                <input v-model="email" id="email" type="email" class="form-control" name="email" value="" required>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="password" class="col-md-4 control-label">Password</label>
-
-            <div class="col-md-6">
-                <input v-model="password" id="password" type="password" class="form-control" name="password" required>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-            <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-md-6 col-md-offset-4">
+            <div class="col-md-7 col-md-offset-4">
                 <button type="submit" class="btn btn-primary">
                     Register
                 </button>
